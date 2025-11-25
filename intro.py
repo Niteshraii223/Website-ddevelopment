bfe56d3bfea65974e71fb4cc10e18b7a9f79a796
Niteshraii223 <raineeta980@gmail.com>
2025-11-25 21:07:49 +0545

    introoo

---
+++ intro.py
@@ -0,0 +1,12 @@
+# Printing my name
+print("My name is Nitesh.")
+# Printing a statement
+print("I love playing games.")
+# printing my age
+print("My age is 15.")
+# printing my hobby
+print("My hobby is coding.")
+# printing sum
+print("The sum of 3 and 6 is", 3 + 6)
+# printing multiplication
+print("The multiple of 5 and 6 is", 5 * 6)
